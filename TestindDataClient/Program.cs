2d0ded9d2466aced7798a3637d7093ecf975a55f
Zyablik1989 <Zyablik1989@yandex.ru>
2021-02-24 17:45:02 +0500

    Added projects for testing

---
+++ TestindDataClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using TestingDataService;
+
+
+namespace TestindDataClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new TestingDataService.TestingDataRetriever.TestingDataRetrieverClient(channel);
+            Console.WriteLine("Имя");
+            var a = Console.ReadLine();
+            Console.WriteLine("Должность 1-3");
+            var b = Console.ReadLine();
+            var position = 0;
+            int.TryParse(b, out position);
+
+            var data = await client.GetTestingDataAsync(
+                new ClientCredentials() {
+
+                Name = a,
+                ClientType = (ClientType)position
+            });
+            Console.WriteLine(
+                new StringBuilder()
+                    .Append($"Lambda - {data.Lambda}{Environment.NewLine}")
+                    .Append($"Frequency - {data.Frequency}{Environment.NewLine}")
+                    .Append($"Data - {data.Data}{Environment.NewLine}")
+                    .Append($"Comment - {data.Comment}{Environment.NewLine}")
+                    .ToString());
+            //Console.WriteLine("Message from Chat: " + reply.Message);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestingDataProcessor/Services/TestingDataHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestingDataService;
+using Grpc.Core;
+
+namespace TestingDataProcessor.Services
+{
+    public class TestingDataHandler : TestingDataRetriever.TestingDataRetrieverBase
+    {
+        public override Task<TestingData> GetTestingData(ClientCredentials client, ServerCallContext context)
+        {
+            return Task.FromResult(ProcessTestingData(client.ClientType));
+        }
+
+        private TestingData ProcessTestingData(ClientType clientType)
+        {
+            var testingData = new TestingData();
+            switch (clientType)
+            {
+                default:
+                    testingData.Comment = "ОШИБКА ДОЛЖНОСТИ";
+                    break;
+                case ClientType.JuniorResearcher:
+                    testingData.SignalType = SignalType.Oss;
+                    break;
+                case ClientType.Researcher:
+                    testingData.SignalType = SignalType.Aws;
+                    break;
+                case ClientType.SeniorResearcher:
+                    testingData.SignalType = SignalType.Murf;
+                    break;
+            }
+
+            if (string.IsNullOrEmpty(testingData.Comment))
+            {
+                testingData.Lambda = (uint)new Random().Next(0,int.MaxValue);
+                testingData.Frequency = (uint)new Random().Next(0, int.MaxValue);
+                testingData.Data = new string(Enumerable.Repeat("0123456789", 4)
+                    .Select(s => s[new Random().Next(s.Length)]).ToArray());
+            }
+
+            return testingData;
+        }
+    }
+
+
+}
