d7f75a4d0c0f786ca5db6389c610451ac39f3e81
Zyablik1989 <Zyablik1989@yandex.ru>
2021-02-28 03:52:22 +0500

    MVVM basic added

---
+++ TestingDataWPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using OxyPlot;
+
+namespace TestingDataWPF.ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        #region INotify... realisation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var changed = PropertyChanged;
+
+            changed?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetProperty<T>(ref T backingStore, T value,
+            [CallerMemberName] string propertyName = "",
+            Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        #endregion
+
+        public ICommand RetrieveDataCommand { get; set; }
+
+        //Title of window
+        string title = "Testing data retriever";
+        public string Title
+        {
+            get { return title; }
+            set { SetProperty(ref title, value); }
+        }
+
+        #region Tesing data fields
+        //Lambda
+        string lambda = string.Empty;
+        public string Lambda
+        {
+            get { return lambda; }
+            set { SetProperty(ref lambda, value); }
+        }
+
+        //Frequency
+        string frequency = string.Empty;
+        public string Frequency
+        {
+            get { return frequency; }
+            set { SetProperty(ref frequency, value); }
+        }
+
+        //Frequency
+        string signalType = string.Empty;
+        public string SignalType
+        {
+            get { return signalType; }
+            set { SetProperty(ref signalType, value); }
+        }
+
+        PlotModel plotModel = new PlotModel();
+        public PlotModel PlotModel
+        {
+            get { return plotModel; }
+            set { SetProperty(ref plotModel, value); }
+        }
+
+        //Frequency
+        string comment = string.Empty;
+        public string Comment
+        {
+            get { return comment; }
+            set { SetProperty(ref comment, value); }
+        }
+        #endregion
+    }
+}
---
+++ TestingDataWPF/Views/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TestingDataWPF.ViewModels;
+
+
+namespace TestingDataWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private readonly MainViewModel viewModel = new MainViewModel();
+
+        private void Init()
+        {
+            DataContext = viewModel;
+        }
+
+        //private void RetrieveData(object sender, RoutedEventArgs e)
+        //{
+        //    //Tightly connected test of basic logic
+        //    //var testingdata = DataRetriever.CollectTestingData(tbName.Text, cbPosition.SelectedIndex);
+
+        //    ////Drawing plot
+        //    //plotView.Model = PlotModelDefine.GridLinesHorizontal(testingdata.Data);
+
+        //    ////Filling fields
+        //    //tbLambda.Text = testingdata.Lambda.ToString();
+        //    //tbFrequency.Text = testingdata.Frequency.ToString();
+        //    //tbSignalType.Text = testingdata.SignalType.ToString();
+        //    //tbComment.Text = testingdata.Comment;
+        //}
+    }
+}
