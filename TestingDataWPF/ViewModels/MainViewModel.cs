25ddb878bfc745d406b9ff9d47dabee7bfc66810
Zyablik1989 <Zyablik1989@yandex.ru>
2021-03-04 21:58:38 +0500

    #endregion was lost

--- TestingDataWPF/ViewModels/MainViewModel.cs
+++ TestingDataWPF/ViewModels/MainViewModel.cs
@@ -38,6 +38,8 @@ namespace TestingDataWPF.ViewModels
             return true;
         }
 
+        #endregion
+
         #region UI fields
 
         string title = ResourceHandler.GetResource("TestingdatareceiverString");
